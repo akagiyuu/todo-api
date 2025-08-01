d701fc7f53591f1d0c65ecc2705659bf4fc7ba35
akagiyuu <khanghuynh0245@gmail.com>
2025-08-02 00:28:27 +0700

    fix(swagger): parse dep and internal

--- script/build.sh
+++ script/build.sh
@@ -3,5 +3,5 @@
 OUTPUT="${1:-main}"
 
 sqlc generate -f internal/database/sqlc.yml
-swag init -d cmd/api,internal/handler
+swag init -d cmd/api,internal/handler --parseDependency --parseInternal
 go build -o $OUTPUT cmd/api/main.go
