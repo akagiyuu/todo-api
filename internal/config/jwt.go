a5a2aecfc4c1475fcf66c5f624234ae4f1c03620
akagiyuu <khanghuynh0245@gmail.com>
2025-08-02 01:56:39 +0700

    fix(jwt): secret from []byte instead

--- internal/config/jwt.go
+++ internal/config/jwt.go
@@ -1,6 +1,6 @@
 package config
 
 type JwtConfig struct {
-	Secret    string `env:"JWT_SECRET" envDefault:"secret"`
+	Secret    []byte `env:"JWT_SECRET" envDefault:"secret"`
 	ExpiredIn int    `env:"JWT_EXPIRED_IN" envDefault:"24"` // hour
 }
